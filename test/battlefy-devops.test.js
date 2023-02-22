6192d8e93d04dc1cb6f0a8232e011c416c3d385b
Bo Peng <bopeng48@gmail.com>
2023-02-21 16:33:40 -0800

    Initial commit

---
+++ test/battlefy-devops.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const BattlefyDevops = require('../lib/battlefy-devops-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/battlefy-devops-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new BattlefyDevops.BattlefyDevopsStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
