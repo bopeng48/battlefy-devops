0c8d37296d1862eb77461f4a919930cc3325ff12
Bo Peng <bopeng48@gmail.com>
2023-02-24 17:47:24 -0800

    cleaned up lambda code

--- lambda/url-shortener/index.js
+++ lambda/url-shortener/index.js
@@ -4,13 +4,11 @@ const AWS = require('aws-sdk');
 const Redis = require("ioredis");
 const crypto = require('crypto');
 
-
 const client = new Redis({
     port: 6379,
     host: "clustercfg.test.dubowl.memorydb.us-west-2.amazonaws.com",
     tls: {},
-});;
-
+});
 
 exports.handler = async function(event) {
     try {
@@ -37,12 +35,11 @@ exports.handler = async function(event) {
                     body: 'Missing required parameter: url'
                 };
             }
-
             const id = crypto.randomBytes(4).toString('hex');
             await client.set(id, requestBody.longUrl);
             return {
                 statusCode: 200,
-                body: 'This is a POST request'
+                body: id
             };
         } else {
             return {
